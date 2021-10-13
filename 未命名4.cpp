1f65fa195f781de06850982e29d507cf125686aa
KuN <91597213+Destinkun@users.noreply.github.com>
2021-10-13 16:18:29 +0800

    Update 未命名4.cpp

--- 未命名4.cpp
+++ 未命名4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+//递归
 int f(int n)
 {
 
@@ -7,6 +8,23 @@ int f(int n)
 	if (n==1) return 1;
 	return f(n-1)+f(n-2);
 }
+ //递推方法
+  int fun（int n）
+        if(n==1)return 1;
+        if(n==2)return 1;
+
+        int Part1 = 1;
+        int Part2 = 1;
+        int temp;
+        int sum = 0;
+
+        for (int i = 3; i <= n; i++) {
+            sum = Part1+Part2;
+            temp = Part2;
+            Part2 = sum;
+            Part1 = temp;
+        }
+        return sum;
 int main()
 {
 	cout<<f(6);
